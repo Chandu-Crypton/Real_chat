85cf50a718b5cc314702ad01f85abec255bf61e9
Chandu-Crypton <138280985+Chandu-Crypton@users.noreply.github.com>
2025-04-21 13:18:24 +0530

    Update socket.js

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const URL =
     import.meta.env.MODE === "production"
-        ? "https://mern-chat-socket.onrender.com"
+        ? "https://real-chat-qqz0.onrender.com
         : "http://localhost:3000";
 
 export const socket = io(URL);
